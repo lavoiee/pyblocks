7419e0f7d04701a3761c95b3d96678cb0a4a7093
Eric <ericjohnlavoie@gmail.com>
2020-03-10 00:47:32 -0400

    Added ANSI escape sequences to output for displaying blocks

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import hashlib
 import json
+import colors
+
 
 class Block():
+
     def __init__(self, nonce, tstamp, transaction, prevhash=''):
         self.nonce = nonce
         self.tstamp = tstamp
@@ -15,27 +18,39 @@ class Block():
         return hashlib.sha256(block_string).hexdigest()
 
     def __str__(self):
-        string = "nonce :" + str(self.nonce)
-        string += "tstamp :" + str(self.tstamp)
-        string += "transaction :" + str(self.transaction)
-        string += "prevhash: " + str(self.prevhash)
-        string += "hash: " + str(self.hash)
+
+        string = "\033[01m" + "\033[32m" + "nonce :" + "\033[0m" + str(self.nonce) + "\n"
+        string += "\033[01m" + "\033[32m" + "tstamp :" + "\033[0m" + str(self.tstamp) + "\n"
+        string += "\033[01m" + "\033[32m" + "transaction :" + "\033[0m" + str(self.transaction) + "\n"
+        string += "\033[01m" + "\033[32m" + "prevhash: " + "\033[36m" + str(self.prevhash) + "\n"
+        string += "\033[01m" + "\033[32m" + "hash: " + "\033[36m" + str(self.hash) + "\n"
+
+        return string
+
 
 class BlockChain():
     def __init__(self):
         self.chain = [self.generateGenesisBlock(), ]
+
     def generateGenesisBlock(self):
-        return Block(1, '03/09/2020', "In order for a slave to be free he must leave the plantation")
+        return Block(1, "03/09/2020", "In order for a slave to be free he must leave the plantation")
+
     def getLastBlock(self):
         return self.chain[-1]
+
     def addBlock(self, newBlock):
-        newBlock.prevhash = self.getLastBlock().hash()
+        newBlock.prevhash = self.getLastBlock().hash
         newBlock.hash = newBlock.calchash()
         self.chain.append(newBlock)
 
-blockFundCoin = BlockChain()
 
-blockFundCoin.addBlock(Block(1, '03/09/2020', 1100))
-blockFundCoin.addBlock(Block(1, '03/10/2020', 500))
-blockFundCoin.addBlock(Block(1, '03/11/2020', 1800))
+blockFund = BlockChain()
 
+blockFund.addBlock(Block(1, "03/09/2020", 1100))
+
+blockFund.addBlock(Block(1, "03/10/2020", 500))
+
+blockFund.addBlock(Block(1, "03/11/2020", 1800))
+
+for b in blockFund.chain:
+    print(b)
